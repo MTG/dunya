085e1760577096a1f2d7681234e8577c1a160a50
Alastair Porter <alastair.porter@upf.edu>
2012-11-08 12:19:10 +0100

    cmbrowser project and stub apps

---
+++ cmbrowser/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import patterns, include, url, static
+
+from django.conf import settings
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', '.views.main', name='main'),
+    # url(r'^browser/', include('browser.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+#if settings.DEBUG:
+#    urlpatterns += static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ fabfile.py
@@ -0,0 +1,19 @@
+from fabric.api import *
+
+def up(port="8001"):
+    local("python manage.py runserver 0.0.0.0:%s"%port)
+
+def reset():
+    with settings(warn_only=True):
+        local("rm db.sqlite")
+    local("python manage.py syncdb --noinput")
+
+def dump():
+    with hide('running', 'status'):
+        local("python manage.py dumpdata browser --indent=4")
+
+def celery():
+    local("python manage.py celeryd -E --autoreload --loglevel=DEBUG")
+
+def test():
+    local("python manage.py test browser")
