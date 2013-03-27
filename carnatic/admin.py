90068b07ca46b4ee4e277fd4e53830d87bf23191
Alastair Porter <alastair.porter@upf.edu>
2013-03-27 15:59:41 +0100

    Initial start for search forms

---
+++ carnatic/admin.py
@@ -0,0 +1,17 @@
+from carnatic import models
+from django.contrib import admin
+
+admin.site.register(models.GeographicRegion)
+admin.site.register(models.Artist)
+admin.site.register(models.Concert)
+admin.site.register(models.Raaga)
+admin.site.register(models.Taala)
+admin.site.register(models.Work)
+admin.site.register(models.WorkAttribute)
+admin.site.register(models.WorkAttributeType)
+admin.site.register(models.WorkAttributeTypeValue)
+admin.site.register(models.Recording)
+admin.site.register(models.Instrument)
+admin.site.register(models.InstrumentPerformance)
+admin.site.register(models.Composer)
+admin.site.register(models.Location)
---
+++ carnatic/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+class ArtistSearchForm(forms.Form):
+    pass
+
+class WorkSearchForm(forms.Form):
+    pass
+class InstrumentSearchForm(forms.Form):
+    pass
+class RaagaSearchForm(forms.Form):
+    pass
+class TaalaSearchForm(forms.Form):
+    pass
