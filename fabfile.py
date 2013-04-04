82f78eb49b2c78cd2fefae93dd393a197f723d72
Alastair Porter <alastair.porter@upf.edu>
2013-04-04 17:03:10 +0200

    clean up fabfile

--- fabfile.py
+++ fabfile.py
@@ -4,16 +4,12 @@ def up(port="8001"):
     local("python manage.py runserver 0.0.0.0:%s"%port)
 
 def reset():
-    with settings(warn_only=True):
-        local("rm db.sqlite")
     local("python manage.py syncdb --noinput")
 
 def dump():
     with hide('running', 'status'):
-        local("python manage.py dumpdata browser --indent=4")
-
-def celery():
-    local("python manage.py celeryd -E --autoreload --loglevel=DEBUG")
+        local("python manage.py dumpdata data data.SourceName --indent=4")
+        local("python manage.py dumpdata carnatic carnatic.Instrument carnatic.Taala carnatic.Raaga --indent=4")
 
 def test():
     local("python manage.py test browser")
