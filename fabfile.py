6c53363a9a5e6684a0ec0a6ef0013481c201c76e
Alastair Porter <alastair.porter@upf.edu>
2013-04-09 15:24:32 +0200

    fab commands

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,5 @@
 from fabric.api import *
+import os
 
 def up(port="8001"):
     local("python manage.py runserver 0.0.0.0:%s"%port)
@@ -6,10 +7,26 @@ def up(port="8001"):
 def reset():
     local("python manage.py syncdb --noinput")
 
-def dump():
+def dumpfixture(modname):
     with hide('running', 'status'):
-        local("python manage.py dumpdata data data.SourceName --indent=4")
-        local("python manage.py dumpdata carnatic carnatic.Instrument carnatic.Taala carnatic.Raaga --indent=4")
+        redir = "%s/fixtures/initial_data.json" % modname
+        if modname == "data":
+            local("python manage.py dumpdata data data.SourceName --indent=4 > %s" % redir)
+        elif modname == "carnatic": 
+            local("python manage.py dumpdata carnatic carnatic.Instrument carnatic.Taala carnatic.Raaga carnatic.GeographicRegion --indent=4 > %s" % redir)
+
+def dumpdata(fname="dunya_data.json"):
+    with hide('running', 'status'):
+        modules = ["browse", "carnatic", "data", "docserver", "social"]
+        local("python manage.py dumpdata --indent=4 %s > %s" % (" ".join(modules), fname))
+        print "dumped data to %s" % fname
+
+def loaddata(fname="dunya_data.json"):
+    print "Loading data from %s" % fname
+    if os.path.exists(fname):
+        local("python manage.py loaddata %s" % fname)
+    else:
+        print " (missing!)"
 
 def test():
     local("python manage.py test browser")
