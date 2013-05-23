855f00f8deadf6d5a24745c42931ffd3eab262b7
Alastair Porter <alastair.porter@upf.edu>
2013-05-23 17:07:17 +0200

    add migrations

--- fabfile.py
+++ fabfile.py
@@ -5,11 +5,12 @@ def up(port="8001"):
     local("python manage.py runserver 0.0.0.0:%s"%port)
 
 def setupdb():
+    local("python manage.py syncdb --noinput")
     local("python manage.py migrate kombu.transport.django")
     local("python manage.py migrate djcelery")
-    local("python manage.py syncdb --noinput")
     local("python manage.py migrate data")
     local("python manage.py migrate carnatic")
+    local("python manage.py migrate dashboard")
 
 def updatedb():
     with settings(warn_only=True):
