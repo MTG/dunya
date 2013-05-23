b3858143a917d54b33c4d4f697ac99ff65e0a7b1
Mohamed Sordo <mohamed.sordo@gmail.com>
2013-05-23 12:41:20 +0200

    added comments and auth to dumpdata in fabfile

--- fabfile.py
+++ fabfile.py
@@ -35,7 +35,7 @@ def dumpfixture(modname):
 
 def dumpdata(fname="dunya_data.json"):
     with hide('running', 'status'):
-        modules = ["browse", "carnatic", "data", "docserver", "social"]
+        modules = ["browse", "carnatic", "data", "docserver", "social", "comments", "auth"]
         local("python manage.py dumpdata --indent=4 %s > %s" % (" ".join(modules), fname))
         print "dumped data to %s" % fname
 
