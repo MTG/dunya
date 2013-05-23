4c9f5e5e270cce97bb250cbbe10e9eb4800c0e5e
Alastair Porter <alastair.porter@upf.edu>
2013-05-23 17:11:13 +0200

    Fix migrations

--- data/migrations/0001_initial.py
+++ data/migrations/0001_initial.py
@@ -24,6 +24,31 @@ class Migration(SchemaMigration):
         ))
         db.send_create_signal(u'data', ['Source'])
 
+        # Adding model 'Reference'
+        db.create_table(u'data_reference', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('source_name', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['data.SourceName'])),
+            ('uri', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('last_updated', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'data', ['Reference'])
+
+        # Adding model 'Label'
+        db.create_table(u'data_label', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('source', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['data.Source'], null=True, blank=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+        ))
+        db.send_create_signal(u'data', ['Label'])
+
+        # Adding M2M table for field references on 'Label'
+        db.create_table(u'data_label_references', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('label', models.ForeignKey(orm[u'data.label'], null=False)),
+            ('reference', models.ForeignKey(orm[u'data.reference'], null=False))
+        ))
+        db.create_unique(u'data_label_references', ['label_id', 'reference_id'])
+
 
     def backwards(self, orm):
         # Deleting model 'SourceName'
@@ -32,8 +57,31 @@ class Migration(SchemaMigration):
         # Deleting model 'Source'
         db.delete_table(u'data_source')
 
+        # Deleting model 'Reference'
+        db.delete_table(u'data_reference')
+
+        # Deleting model 'Label'
+        db.delete_table(u'data_label')
+
+        # Removing M2M table for field references on 'Label'
+        db.delete_table('data_label_references')
+
 
     models = {
+        u'data.label': {
+            'Meta': {'object_name': 'Label'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'references': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': u"orm['data.Reference']", 'null': 'True', 'blank': 'True'}),
+            'source': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['data.Source']", 'null': 'True', 'blank': 'True'})
+        },
+        u'data.reference': {
+            'Meta': {'object_name': 'Reference'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_updated': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'source_name': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['data.SourceName']"}),
+            'uri': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
         u'data.source': {
             'Meta': {'object_name': 'Source'},
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
