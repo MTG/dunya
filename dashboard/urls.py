cde6ab7e070058fbe58d00c7dbbdbf7730169c1f
Alastair Porter <alastair.porter@upf.edu>
2013-07-04 15:10:42 +0200

    basic overview pages

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -8,5 +8,7 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='dashboard-home'),
     url(r'collection/%s$' % (uuid_match, ), views.collection, name='dashboard-collection'),
     url(r'release/%s$' % (uuid_match, ), views.release , name='dashboard-release'),
+    url(r'directory/(?P<dirid>\d+)$', views.directory , name='dashboard-directory'),
+    url(r'file/(?P<fileid>\d+)$', views.file , name='dashboard-file'),
 )
 
--- dashboard/views.py
+++ dashboard/views.py
@@ -4,6 +4,8 @@ from django.core.urlresolvers import reverse
 
 from dashboard import models
 from dashboard import forms
+import compmusic
+import os
 
 def index(request):
     if request.method == 'POST':
@@ -11,6 +13,9 @@ def index(request):
         if form.is_valid():
             # Import collection id
             coll_id = form.cleaned_data['collectionid']
+            path = form.cleaned_data['path']
+            coll_name = compmusic.musicbrainz.get_collection_name(coll_id)
+            models.Collection.objects.create(id=coll_id, name=coll_name, root_directory=path)
             return HttpResponseRedirect(reverse('dashboard-home'))
     else:
         form = forms.AddCollectionForm()
@@ -28,7 +33,35 @@ def collection(request, uuid):
     return render(request, 'dashboard/collection.html', ret)
 
 def release(request, uuid):
-    return render(request, 'dashboard/release.html')
+    release = models.MusicbrainzRelease.objects.get(pk=uuid)
+    files = release.collectiondirectory_set.all()
 
-def recording(request, recordingid):
-    pass
+    ret = {"release": release, "files": files}
+    return render(request, 'dashboard/release.html', ret)
+
+def file(request, fileid):
+    return render(request, 'dashboard/file.html')
+
+def directory(request, dirid):
+    """ A directory that wasn't matched to a release in the collection.
+    This could be because it has no release tags, or the release isn't in
+    the collection.
+    We want to group together as much common information as possible, and
+    link to musicbrainz if we can.
+    """
+
+    directory = models.CollectionDirectory.objects.get(pk=dirid)
+    collection = thedir.collection
+    full_path = os.path.join(collection.root_directory, thedir.path)
+    files = os.listdir(full_path)
+    meta = {}
+    releaseids = []
+    releasename = []
+    artistids = []
+    artistname = []
+    for f in files:
+        data = compmusic.file_metadata(os.path.join(full_path, f))
+        meta[f] = data
+
+    ret = {"files": meta, "directory": directory}
+    return render(request, 'dashboard/directory.html', ret)
