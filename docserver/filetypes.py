d4eddc1227170501ebd10d122e27e3ca42bd5f3a
Alastair Porter <alastair.porter@upf.edu>
2013-09-13 17:25:40 +0200

    don't have module in filetypes any more

--- docserver/filetypes.py
+++ docserver/filetypes.py
@@ -6,8 +6,5 @@ class FileType(object):
 class MP3FileType(FileType):
     extension = "mp3"
 
-    def convert_to_wav(self, incoming):
-        return incoming
-
 class CSVFileType(FileType):
     extension = "csv"
--- docserver/management/commands/mkfiletypes.py
+++ docserver/management/commands/mkfiletypes.py
@@ -15,18 +15,18 @@ class Command(BaseCommand):
         for name, ftype in inspect.getmembers(filetypes, inspect.isclass):
             if issubclass(ftype, filetypes.FileType) and not name == "FileType":
                 path = "%s%s" % (prefix, name)
-                all_classes.append(path)
-                if models.SourceFileType.objects.filter(module=path).count() == 0:
+                all_classes.append(ftype.extension)
+                if models.SourceFileType.objects.filter(extension=ftype.extension).count() == 0:
                     if hasattr(ftype, "name"):
                         name = ftype.name
                     else:
                         # If no name, split on camel case
                         s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
                         name = re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
-                    models.SourceFileType.objects.create(module=path, name=name, extension=ftype.extension)
+                    models.SourceFileType.objects.create(name=name, extension=ftype.extension)
         # Remove any items in the DB that no longer have a class
         for i in models.SourceFileType.objects.all():
-            if i.module not in all_classes:
+            if i.extension not in all_classes:
                 print "deleting", i
-                models.FileType.objects.filter(module=i.module).delete()
+                models.FileType.objects.filter(extension=i.extension).delete()
 
