5e50204f12d864af444bcb00277e37289895ebb7
Alastair Porter <alastair.porter@upf.edu>
2013-09-16 17:35:38 +0200

    make admin and forms reflect models

--- docserver/forms.py
+++ docserver/forms.py
@@ -6,10 +6,6 @@ class EssentiaVersionForm(forms.ModelForm):
     class Meta:
         model = models.EssentiaVersion
 
-class WorkerMachineForm(forms.ModelForm):
-    class Meta:
-        model = models.WorkerMachine
-
 class ModuleForm(forms.ModelForm):
     class Meta:
         model = models.Module
