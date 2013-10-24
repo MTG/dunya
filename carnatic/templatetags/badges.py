4c24cfa81a6b6b711b659eb84063fb4758853d29
Alastair Porter <alastair.porter@upf.edu>
2013-10-24 14:40:58 +0200

    fill in some badges

--- carnatic/templatetags/badges.py
+++ carnatic/templatetags/badges.py
@@ -4,73 +4,41 @@ from carnatic.models import *
 
 register = template.Library()
 
-def get_image(entity, noimage):
-    if entity.images.count():
-        image = entity.images.all()[0].image.url
-    else:
-        image = "/media/images/%s.jpg" % noimage
-    return image
-
-
 @register.inclusion_tag("badges/similar_concert.html")
 def badge_similar_concert(concert):
     if not isinstance(concert, Concert):
         concert = Concert.objects.get(pk=concert)
-    return {"title": concert.title,
-            "artist": concert.artistnames(),
-            "detail": None,
-            "date": concert.year,
-            "url": concert.get_absolute_url(),
-            "image": get_image(concert, "noconcert")
-           }
+    return {"concert": concert}
 
 @register.inclusion_tag("badges/concert.html")
 def badge_concert(concert):
     if not isinstance(concert, Concert):
         concert = Concert.objects.get(pk=concert)
-    return {"concert": concert
-           }
+    return {"concert": concert}
 
 @register.inclusion_tag("badges/artist.html")
 def badge_performance(performance):
     if not isinstance(performance, InstrumentPerformance):
         performance = InstrumentPerformance.objects.get(pk=performance)
-    artist = performance.performer
-    if artist.images.count():
-        image = artist.images.all()[0].image.url
-    else:
-        image = "/media/images/noartist.jpg"
-    return {"performance": performance,
-            "image": image
-           }
+    return {"performance": performance}
 
 @register.inclusion_tag("badges/artist.html")
 def badge_artist(artist):
     if not isinstance(artist, Artist):
         artist = Artist.objects.get(pk=artist)
-    perfs = artist.instrumentperformance_set.all()
-    if perfs:
-        performance = perfs[0]
-    else:
-        performance = None
-    return {"artist": artist
-           }
+    return {"artist": artist}
 
 @register.inclusion_tag("badges/recording.html")
 def badge_recording(recording):
     if not isinstance(recording, Recording):
         recording = Recording.objects.get(pk=recording)
-    return {"recording": recording
-           }
+    return {"recording": recording}
 
 @register.inclusion_tag("badges/instrument.html")
 def badge_instrument(instrument):
     if not isinstance(instrument, Instrument):
         instrument = Instrument.objects.get(pk=instrument)
-    return {"instrument": instrument,
-            "image": get_image(instrument, "noinstrument")
-           }
-
+    return {"instrument": instrument}
 
 @register.inclusion_tag("badges/sample.html")
 def badge_sample(sample):
