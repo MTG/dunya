dbd3fdd2d0bf8dd3ade9cb89b0e5a08a4324b827
Alastair Porter <alastair.porter@upf.edu>
2013-10-31 00:19:57 +0100

    fix and run instrument matcher

--- carnatic/management/commands/matchinstruments.py
+++ carnatic/management/commands/matchinstruments.py
@@ -9,13 +9,24 @@ from django.conf import settings
 import data
 import requests
 
+def unicode_csv_reader(unicode_csv_data, dialect=csv.excel, **kwargs):
+    # csv.py doesn't do Unicode; encode temporarily as UTF-8:
+    csv_reader = csv.reader(utf_8_encoder(unicode_csv_data),
+                            dialect=dialect, **kwargs)
+    for row in csv_reader:
+        # decode UTF-8 back to Unicode, cell by cell:
+        yield [unicode(cell, 'utf-8') for cell in row]
+
+def utf_8_encoder(unicode_csv_data):
+    for line in unicode_csv_data:
+        yield line.encode('utf-8')
+
 class Command(BaseCommand):
     help = "Load ajay's updates to instruments"
 
     def handle(self, *args, **options):
         fname = args[0]
 
-        #TODO: utf-8 in csv?
         reader = csv.reader(open(fname, "rb"))
         reader.next() # header
         for row in reader:
@@ -26,28 +37,29 @@ class Command(BaseCommand):
             altpicture = row[6]
             altsource = row[7]
             source = row[8]
-            description = row[9]
+            description = row[9].decode("utf-8")
+
+            print name
 
             i = models.Instrument.objects.get(name=name)
             i.is_percussion = not not percussion
             if not picture and altpicture:
                 picture = altpicture
             if picture:
+                print "downloading picture from", picture
                 req = requests.get(picture)
-                piccontents = req.contents
-                im = data.models.image()
-                im.image.save("%s.jpg" % i.name, contentfile(piccontents))
-                # TODO: Clear images
+                piccontents = req.content
+                im = data.models.Image()
                 i.images.remove()
+                im.image.save("%s.jpg" % i.name, ContentFile(piccontents))
                 i.images.add(im)
+            if description:
                 if source:
-                    sn = data.models.SourceName.objects.get(name="kutcheris.com")
-                    sourceob, created = data.models.Source.objects.get_or_create(source_name=sn, uri=link, defaults={"title": a.name})
+                    sn = data.models.SourceName.objects.get(name="Wikipedia")
+                    sourceob, created = data.models.Source.objects.get_or_create(source_name=sn, uri=source, defaults={"title": name})
                 else:
                     sourceob = None
                 descriptionob = data.models.Description.objects.create(description=description, source=sourceob)
                 i.description = descriptionob
                 i.save()
-
-
 
