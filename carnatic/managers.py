0c9dcccc1cac462609208e0ffbe4ffe8b46d8e42
Alastair Porter <alastair.porter@upf.edu>
2013-11-19 11:23:23 +0100

    when doing fuzzy search for a raaga/taala first do an exact search

--- carnatic/managers.py
+++ carnatic/managers.py
@@ -5,14 +5,17 @@ from compmusic.fuzzy import stringDuplicates
 class FuzzySearchManager(models.Manager):
     def fuzzy(self, name):
         name = name.lower()
-        items = self.model.objects.all()
-        names = [i.name.lower() for i in items]
-        dups = stringDuplicates.stringDuplicates(name, names, stripped=True)
-        if len(dups) != 1:
-            raise self.model.DoesNotExist()
-        n = dups[0]
-        for i in items:
-            if i.name.lower() == n.lower():
-                return i
-        raise Exception("Whoops")
+        try:
+            self.model.objects.get(name__iexact=name)
+        except self.model.DoesNotExist:
+            items = self.model.objects.all()
+            names = [i.name.lower() for i in items]
+            dups = stringDuplicates.stringDuplicates(name, names, stripped=True)
+            if len(dups) != 1:
+                raise self.model.DoesNotExist()
+            n = dups[0]
+            for i in items:
+                if i.name.lower() == n.lower():
+                    return i
+            raise Exception("Whoops")
 
