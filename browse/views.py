9ff02e9feb9d6b5d4640fe0dca5a52e673d428d2
Alastair Porter <alastair.porter@upf.edu>
2013-11-25 16:16:39 +0100

    point / at /carnatic

--- browse/views.py
+++ browse/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def main(request):
+    return redirect("carnatic-main")
     ret = {}
     return render(request, "browse/index.html", ret)
