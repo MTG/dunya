56ff13d7dd16b1d3c639909988ea2e6345698d85
Alastair Porter <alastair.porter@upf.edu>
2014-01-15 13:23:17 +0100

    clean up user menu

--- carnatic/static/carnatic/js/main.js
+++ carnatic/static/carnatic/js/main.js
@@ -25,26 +25,15 @@ $(document).ready(function() {
 //        }
 //    });
 	var myTimer=false;
-	$("#userOptions").hover(function(){ clearTimeout(myTimer);});
-	$("#guestOptions").hover(function(){ clearTimeout(myTimer);});
-    $('#user').click(function() {
-        $("#userOptions").show({easing: "easeInOutQuad"});
-        $(this).find('#userOptions').addClass( "open", globalSpeed, "easeInOutQuad");
+	$("#usermenuoptions").hover(function(){ clearTimeout(myTimer);});
+    $('#usermenu').click(function() {
+        $("#usermenuoptions").show({easing: "easeInOutQuad"});
+        $(this).find('#usermenuoptions').addClass("open", globalSpeed, "easeInOutQuad");
     });
-    $('#user').mouseleave(function() {
+    $('#usermenu').mouseleave(function() {
 		myTimer = setTimeout(function(){
-			$("#userOptions").hide({easing: "easeInOutQuad"});
-			$(this).find('#userOptions').removeClass( "open", globalSpeed, "easeInOutQuad");
-		}, 500)
-    });
-    $('#guest').click(function() {
-        $("#guestOptions").show({easing: "easeInOutQuad"});
-        $(this).find('#guestOptions').addClass( "open", globalSpeed, "easeInOutQuad");
-    });
-    $('#guest').mouseleave(function() {
-		myTimer = setTimeout(function(){
-			$("#guestOptions").hide({easing: "easeInOutQuad"});
-			$(this).find('#guestOptions').removeClass( "open", globalSpeed, "easeInOutQuad");
+			$("#usermenuoptions").hide({easing: "easeInOutQuad"});
+			$(this).find('#usermenuoptions').removeClass("open", globalSpeed, "easeInOutQuad");
 		}, 500)
     });
 
