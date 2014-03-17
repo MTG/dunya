87bc3ea6bd9c7dcb6c6cc522dd078e4752a55e32
Alastair Porter <alastair.porter@upf.edu>
2014-03-17 10:33:57 +0100

    syntax fixes

--- carnatic/views.py
+++ carnatic/views.py
@@ -18,7 +18,6 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.db.models import Q
 
