ed98e9a67d78d8de209dc905c8076ba3f2a2f0a8
Alastair Porter <alastair.porter@upf.edu>
2014-04-23 18:19:19 +0200

    re-generate 0 size mp3s

--- motifdiscovery/tasks.py
+++ motifdiscovery/tasks.py
@@ -30,6 +30,51 @@ def convert_mp3(file_id):
             s.segment_path = outname
             s.save()
             os.unlink(inname)
+@app.task
+def fix_segments(file_id):
+    thefile = models.File.objects.get(pk=file_id)
+    patterns = thefile.pattern_set.all()
+    wav_file, wav_created = util.docserver_get_wav_filename(thefile.mbid)
+    print "Making segments for file %s" % thefile
+    numpat = len(patterns)
+    print "Got %s segments to do" % numpat
+    wav_in = wave.open(wav_file, 'rb')
+    params = wav_in.getparams()
+    samplerate = wav_in.getframerate()
+
+    with transaction.atomic():
+        for i, p in enumerate(patterns, 1):
+            if i % 100 == 0:
+                print "  - [%s]: Done %s/%s" % (file_id, i, numpat)
+            r_start = round(p.start_time, 1)
+            r_end = round(p.end_time, 1)
+            qs = models.Segment.objects.filter(file=thefile, rounded_start=r_start, rounded_end=r_end)
+            if qs.exists():
+                s = qs.get()
+
+                start_str = str(r_start)
+                end_str = str(r_end)
+                fname = "%s-%s.wav" % (start_str, end_str)
+                dname = start_str[0]
+                
+                full_dir = os.path.join(ROOT_DIR, str(thefile.id), dname)
+                full_path = os.path.join(full_dir, fname)
+                mp3_path = full_path[:-3]+"mp3"
+
+                if os.path.exists(mp3_path) and os.stat(mp3_path).st_size == 0:
+                    print "deleting", mp3_path
+                    os.unlink(mp3_path)
+                    sframe = int(math.floor(r_start * samplerate))
+                    eframe = int(math.ceil(r_end * samplerate))
+                    wav_in.setpos(sframe)
+                    frames = wav_in.readframes(eframe-sframe)
+
+                    wav_out = wave.open(full_path, 'wb')
+                    wav_out.setparams(params)
+                    wav_out.writeframes(frames)
+
+    if wav_created:
+        os.unlink(wav_file)
 
 @app.task
 def make_segments(file_id):
