499f4b93affacef9d97451960e1d94fc0af89b1a
darad <dabiri.dara@gmail.com>
2014-05-05 09:18:46 +0200

    Use a single command to get main instrument for both carnatic and hindustani

--- data/management/commands/getmaininstrument.py
+++ data/management/commands/getmaininstrument.py
@@ -16,20 +16,29 @@
 
 from django.core.management.base import BaseCommand, CommandError
 
-from carnatic import models
+from carnatic import models as carnatic_models
+from hindustani import models as hindustani_models
 import collections
 
 class Command(BaseCommand):
     help = "Find each artist's main instrument and fill in the field"
 
     def handle(self, *args, **options):
+        if len(args) == 0 or args[0] not in ('carnatic', 'hindustani'):
+            raise CommandError("""Missing argument. The argument passed to this
+                                command should be one of \'carnatic\' or 
+                                \'hindustani\'i""")
+        models = {  'carnatic':     carnatic_models,
+                    'hindustani':   hindustani_models,
+                 }[args[0]]
         artists = models.Artist.objects.all()
         for a in artists:
             counter = collections.Counter()
             for ip in a.instrumentperformance_set.all():
                 counter[ip.instrument] += 1
-            for ip in a.instrumentconcertperformance_set.all():
-                counter[ip.instrument] += 1
+            if models == carnatic_models:
+                for ip in a.instrumentconcertperformance_set.all():
+                    counter[ip.instrument] += 1
             try:
                 common = counter.most_common(1)[0]
                 inst = common[0]
