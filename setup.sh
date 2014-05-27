c8e962db93fa158b460784ecca6b1c226d6f56a9
Alastair Porter <alastair.porter@upf.edu>
2014-05-27 12:00:15 +0200

    modify installation to put essentia in the virtualenv

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 sudo apt-get install python-numpy python-scipy python-matplotlib libsndfile1-dev lame libjpeg8-dev postgresql-server-dev-all libxml2-dev libxslt1-dev
+sudo apt-get install build-essential libyaml-dev libfftw3-dev libavcodec-dev libavformat-dev python-dev libsamplerate0-dev libtag1-dev python-numpy-dev
 virtualenv env
 . env/bin/activate
 pip install --upgrade setuptools
 pip install --upgrade pip
 pip install python-dateutil
-ln -s /usr/local/lib/python2.7/dist-packages/essentia/ env/lib/python2.7/site-packages
 ln -s /usr/lib/python2.7/dist-packages/numpy env/lib/python2.7/site-packages
 ln -s /usr/share/pyshared/numpy-1.6.1.egg-info env/lib/python2.7/site-packages
 ln -s /usr/lib/python2.7/dist-packages/scipy* env/lib/python2.7/site-packages
@@ -15,3 +15,10 @@ ln -s /usr/lib/pymodules/python2.7/matplotlib* env/lib/python2.7/site-packages/
 ln -s /usr/lib/pymodules/python2.7/pylab* env/lib/python2.7/site-packages/
 pip install -r requirements
 
+pushd ..
+git clone git@github.com:CompMusic/essentia.git
+pushd essentia
+git checkout -t origin/deploy
+./waf configure --mode=release --with-python --prefix=/srv/dunya/env
+./waf -j4
+./waf install
