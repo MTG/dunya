eea3f297cc7eef7e00328570ed28e66ed30b33e0
Alastair Porter <alastair.porter@upf.edu>
2014-07-29 17:29:25 +0200

    test coverage output

--- dunya/test_settings.py
+++ dunya/test_settings.py
@@ -3,3 +3,10 @@ from settings import *
 INSTALLED_APPS = INSTALLED_APPS + ('django_nose', )
 del DATABASES["motif"]
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+NOSE_ARGS = [
+    '--with-xcoverage',
+    '--cover-package=carnatic,dashboard,docserver,hindustani',
+    '--with-xunit',
+    '--cover-erase'
+]
