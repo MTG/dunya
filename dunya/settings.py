7549c0ee8f52ca3576d7c15270c7801c55755c33
Alastair Porter <alastair.porter@upf.edu>
2015-03-25 12:24:51 +0100

    Add request context processor to templates for `path`
    Used in login/logout links

--- dunya/settings.py
+++ dunya/settings.py
@@ -78,6 +78,7 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'django.template.context_processors.static',
                 'django.template.context_processors.tz',
+                'django.template.context_processors.request',
                 'django.contrib.messages.context_processors.messages',
                 'dunya.context_processors.style_context',
             ],
