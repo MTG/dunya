682622d978d5f01e2bf6134cba7e9cfae5d623a4
Alastair Porter <alastair.porter@upf.edu>
2015-04-21 13:33:57 +0200

    when creating a new category, make unique by name, not sourcefiletype

--- kvedit/utils.py
+++ kvedit/utils.py
@@ -1,7 +1,10 @@
-from kvedit.models import Field, Item, Category 
+from kvedit.models import Field, Item, Category
 
-def upload_kvdata(cat_name, source_file_type, items_dic):   
-    category, created = Category.objects.get_or_create(name=cat_name, source_file_type=source_file_type)
+def upload_kvdata(cat_name, source_file_type, items_dic):
+    category, created = Category.objects.get_or_create(name=cat_name, defaults={source_file_type: source_file_type})
+    if category.source_file_type != source_file_type:
+        category.source_file_type = source_file_type
+        category.save()
     # Select the items with the same id
     existing_items = Item.objects.filter(ref__in=items_dic.keys(), category=category)
     for item in existing_items:
@@ -20,7 +23,7 @@ def upload_kvdata(cat_name, source_file_type, items_dic):
         item.reverify = True
         item.save()
         items_dic[item.ref]
-        
+
         del items_dic[item.ref]
     # Save the new Fields and Items
     for ref, fields in items_dic.iteritems():
