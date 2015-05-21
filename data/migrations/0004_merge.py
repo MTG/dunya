7bfcc7841907fed4e53d7b56be9f3b3eeb042140
Andres Ferraro <aferraro@dcodesoft.com>
2015-05-21 20:10:28 -0300

    Generated migration merge files

---
+++ data/migrations/0004_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('data', '0002_auto_20150407_1025'),
+        ('data', '0003_auto_20150409_1322'),
+    ]
+
+    operations = [
+    ]
