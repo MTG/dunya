84e9443b40ed871627bb0ed6319db24b6deccb5c
Andres Ferraro <andres.ferraro@upf.edu>
2015-09-07 16:35:19 +0200

    Changing carnatic raaga API for showing all related recordings

--- carnatic/models.py
+++ carnatic/models.py
@@ -561,7 +561,7 @@ class Raaga(data.models.BaseModel):
         return recordings
 
     def recordings_form(self, form=None):
-        ret = Recording.objects.filter(works__raaga=self)
+        ret = Recording.objects.filter(Q(works__raaga=self) | Q(raaga=self))
         if form:
             ret = ret.filter(forms__name=form)
         return ret.all()
