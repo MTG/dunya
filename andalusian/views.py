5a698c1ac81f1afe8e12ef43cf2869a6990d1221
Andres Ferraro <andres.ferraro@upf.edu>
2016-02-24 12:54:57 +0100

    Andalusian initial commit

--- andalusian/views.py
+++ andalusian/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+
+from andalusian import models
 
 def main(request):
     return render(request, "andalusian/index.html")
+
+def recording(request, uuid, title=None):
+    recording = get_object_or_404(models.Recording, mbid=uuid)
+    
+    ret={
+         "recording": recording,
+        }
+    return render(request, "andalusian/recording.html", ret)
