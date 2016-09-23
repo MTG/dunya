027b1fe917a515bb0838c91af9034ee3e2b5115a
Andres Ferraro <andres.ferraro@upf.edu>
2016-09-23 21:21:39 +0200

    Fix bug on work search

--- makam/views.py
+++ makam/views.py
@@ -194,7 +194,7 @@ def get_works_and_url(artist, form, usul, makam, perf, q, elem=None):
     url = {}
     if q and q!='':
         ids = list(models.Work.objects.unaccent_get(q).values_list('pk', flat=True))
-        recordings = recordings.filter(work__id__in=ids) | recordings.filter(title__contains=q)
+        recordings = recordings.filter(works__id__in=ids) | recordings.filter(title__contains=q)
 
     if elem != "artist":
         if artist and artist != '':
