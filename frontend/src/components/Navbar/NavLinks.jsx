bd53d28917217bc62a02684f2b997fdf2e046723
giuband <giuseppe.bandiera@live.com>
2016-10-02 14:00:46 +0200

    Add login/logout/profile links in navbar

--- frontend/src/components/Navbar/NavLinks.jsx
+++ frontend/src/components/Navbar/NavLinks.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
+import { LOGIN_URL, LOGOUT_URL, USER_PROFILE_BASE_URL } from 'settings';
 
-const links = [
-  {
-    link: 'https://dunya.compmusic.upf.edu/about/contact',
-    name: 'Contact',
-  },
-  {
-    link: 'https://dunya.compmusic.upf.edu/about/terms',
-    name: 'About',
-  },
-  {
-    link: 'https://dunya.compmusic.upf.edu/social/register',
-    name: 'Register',
-  },
-];
+const buildLink = (name, link) => ({ link, name });
+
+const getLinks = () => {
+  const isUserLoggedIn = Boolean(window.username);
+  const contact = buildLink('Contact', 'https://dunya.compmusic.upf.edu/about/contact');
+  const about = buildLink('About', 'https://dunya.compmusic.upf.edu/about/terms');
+  const register = buildLink('Register', 'https://dunya.compmusic.upf.edu/social/register');
+  const login = buildLink('Login', LOGIN_URL);
+  const logout = buildLink('Logout', LOGOUT_URL);
+  const user = buildLink(window.username, `${USER_PROFILE_BASE_URL}/${window.username}`);
+  if (isUserLoggedIn) {
+    return [contact, about, user, logout];
+  }
+  return [contact, about, register, login];
+};
 
 export default () => (
   <nav className="NavLinks">
     <ul>
-      {links.map((link) => (
+      {getLinks().map(link => (
         <li key={link.name}><a className="NavLinks__item" href={link.link}>{link.name}</a></li>
       ))}
     </ul>
--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -1,17 +1,21 @@
 // API address to get data to fill up filters section
 export const FILTERS_DATA_URL = {
-  carnatic: 'http://dunya.compmusic.upf.edu/new/filters.json',
+  carnatic: 'http://aporter.ca.upf.edu:8001/new/filters.json',
   hindustani: 'https://whatever',
 };
 
 export const SEARCH_URL = {
-  carnatic: 'https://whatever',
+  carnatic: 'http://aporter.ca.upf.edu:8001/new/carnatic/search',
   hindustani: 'https://whatever',
 };
 
 export const AUTOCOMPLETE_URL = {
-  carnatic: undefined,
+  carnatic: 'tests',
 };
+
+export const LOGIN_URL = '';
+export const LOGOUT_URL = '';
+export const USER_PROFILE_BASE_URL = '';
 
 // whether to show in selected list only entries that fulfill all filters
 export const SHOW_ONLY_VISIBLE_SELECTED = false;
