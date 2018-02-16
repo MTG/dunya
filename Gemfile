27e887b87532e74b6fd78fd85f5b73bf133af0f3
Alastair Porter <alastair.porter@upf.edu>
2018-02-16 11:20:59 +0100

    Remove unneded files

--- Gemfile
+++
@@ -1,7 +0,0 @@
-source 'https://rubygems.org'
-
-group :development do
-      gem 'guard'
-      gem 'guard-less'
-      gem 'therubyracer'
-end
--- fabfile.py
+++ fabfile.py
@@ -1,16 +1,8 @@
-from fabric.api import local, settings, env, roles, run, cd, hide, shell_env
+from __future__ import print_function
+
+from fabric.api import local, hide
 import os
 
-env.roledefs["web"] = ["sitar.s.upf.edu"]
-env.roledefs["workers"] = ["itri.s.upf.edu", "devaraya.s.upf.edu", "kora.s.upf.edu", "guqin.s.upf.edu"]
-env.use_ssh_config = True
-env.forward_agent = True
-
-def up(port="8001"):
-    local("python manage.py runserver 0.0.0.0:%s"%port)
-
-def celery():
-    local("celery worker --app=dunya -l info")
 
 def test(module=None, keepdb=True):
     command = "python manage.py test --settings=dunya.test_settings --noinput"
@@ -19,79 +11,13 @@ def test(module=None, keepdb=True):
     if module:
         command += " %s" % module
     if keepdb:
-        print "Keeping DB (if available), run cleantest to recreate schema"
+        print("Keeping DB (if available), run cleantest to recreate schema")
     local(command)
+
 
 def cleantest(module=None):
     test(module, False)
 
-@roles("workers")
-def pipinstall():
-    """Install new dependencies added to requirements"""
-    env.user = "mtg_dunya"
-    with cd("/srv/dunya"):
-        run("env/bin/pip install -r requirements")
-
-    run("sudo supervisorctl restart dunya")
-
-
-@roles("web")
-def updateweb():
-    """Update the webserver"""
-    env.user = "mtg_dunya"
-    with cd("/srv/dunya"):
-        run("git pull", pty=False)
-        # compile and compress less
-        # compress javascript
-        run("env/bin/python manage.py collectstatic --noinput")
-
-        run("env/bin/lessc carnatic/static/carnatic/css/main.less static/carnatic/css/main.css")
-        run("env/bin/lessc carnatic/static/carnatic/css/browse.less static/carnatic/css/browse.css")
-        run("env/bin/lessc carnatic/static/carnatic/css/recording.less static/carnatic/css/recording.css")
-        run("env/bin/lessc carnatic/static/carnatic/css/pages.less static/carnatic/css/pages.css")
-        run("env/bin/lessc carnatic/static/carnatic/css/presentation.less static/carnatic/css/presentation.css")
-
-        run("env/bin/lessc makam/static/makam/css/main.less static/makam/css/main.css")
-        run("env/bin/lessc makam/static/makam/css/browse.less static/makam/css/browse.css")
-        run("env/bin/lessc makam/static/makam/css/recording.less static/makam/css/recording.css")
-        run("env/bin/lessc makam/static/makam/css/pages.less static/makam/css/pages.css")
-        run("env/bin/lessc makam/static/makam/css/presentation.less static/makam/css/presentation.css")
-
-    with cd("/srv/dunya/env/src/pycompmusic"):
-        run("git pull", pty=False)
-
-    run("sudo supervisorctl restart dunya")
-
-@roles("workers")
-@roles("web")
-def essentia(branch=None):
-    """Update essentia on all workers"""
-    with cd("/srv/essentia"):
-        run("git pull", pty=False)
-        run("./waf -j4")
-        run("./waf install")
-
-@roles("workers")
-def updatecelery():
-    env.user = "mtg_dunya"
-    """Update the code for celery on all workers and restart"""
-    with cd("/srv/dunya"):
-        run("git pull", pty=False)
-    with cd("/srv/dunya/env/src/pycompmusic"):
-        run("git pull", pty=False)
-    run("sudo supervisorctl restart dunyacelery")
-
-def setupdb():
-    """ Run this when you are setting up a new installation
-        or have deleted your database
-    """
-    local("python manage.py migrate")
-
-def updatedb():
-    """ Run this when someone has committed some changes to the
-        database migration scripts
-    """
-    local("python manage.py migrate")
 
 def dumpfixture(modname):
     redir_base = "%s/fixtures/%s_%%s.json" % (modname, modname)
@@ -120,15 +46,17 @@ def dumpfixture(modname):
         redir = redir_base % filename
         local("python manage.py makefixture %s --indent=4 > %s" % (modellist, redir))
 
+
 def dumpdata(fname="dunya_data.json"):
     with hide('running', 'status'):
         modules = ["carnatic", "data", "docserver", "account", "auth", "dashboard"]
         local("python manage.py dumpdata --indent=4 %s > %s" % (" ".join(modules), fname))
-        print "dumped data to %s" % fname
+        print("dumped data to %s" % fname)
+
 
 def loaddata(fname="dunya_data.json"):
-    print "Loading data from %s" % fname
+    print("Loading data from %s" % fname)
     if os.path.exists(fname):
         local("python manage.py loaddata %s" % fname)
     else:
-        print " (missing!)"
+        print(" (missing!)")
