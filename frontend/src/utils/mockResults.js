63c30a438886392d3fde88c2da2be2ec17e856e7
Alastair Porter <alastair.porter@upf.edu>
2018-02-21 14:03:45 +0100

    Remove unused mock methods and data

--- frontend/src/utils/mockResults.js
+++
@@ -1,9 +0,0 @@
-import dunyaRecordings from './dunya-recordings.json';
-
-const resultsPerPage = 20;
-
-export const getResults = (pageIndex = 0) => new Promise((resolve) => {
-  const results = dunyaRecordings.slice(resultsPerPage * pageIndex,
-    resultsPerPage * (pageIndex + 1));
-  resolve({ results, moreResults: 28, linkToNext: '' });
-});
