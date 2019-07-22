69017e5cc222b41e313202e124c09fef4b87fa20
Alastair Porter <alastair.porter@upf.edu>
2019-07-22 11:34:13 +0200

    Update tests with changes to _get_mp3_files

--- dashboard/test/test_import.py
+++ dashboard/test/test_import.py
@@ -20,8 +20,10 @@ class CollectionTest(TestCase):
 
     @mock.patch("compmusic.file_metadata")
     @mock.patch("os.listdir")
-    def test_check_existing_dirs_file_removed(self, listdir, meta):
+    @mock.patch("dashboard.jobs._get_mp3_files")
+    def test_check_existing_dirs_file_removed(self, get_mp3_files, listdir, meta):
         listdir.return_value = ["one.mp3"]
+        get_mp3_files.return_value = ["one.mp3"]
         meta.return_value = {"meta": {"recordingid": self.file1id}}
 
         self.assertEqual(2, len(self.directory.collectionfile_set.all()))
@@ -32,8 +34,12 @@ class CollectionTest(TestCase):
     @mock.patch("compmusic.file_metadata")
     @mock.patch("dashboard.jobs._match_directory_to_release")
     @mock.patch("os.listdir")
-    def test_check_existing_dirs_file_added(self, listdir, match, meta):
-        listdir.return_value = ["one.mp3", "two.mp3", "three.mp3"]
+    @mock.patch("dashboard.jobs._get_mp3_files")
+    def test_check_existing_dirs_file_added(self, get_mp3_files, listdir, match, meta):
+        files = ["one.mp3", "two.mp3", "three.mp3"]
+        get_mp3_files.return_value = files
+        listdir.return_value = files
+
         ids = {"/a/directory/audio/subdir/one.mp3": {"meta": {"recordingid": self.file1id}},
                "/a/directory/audio/subdir/two.mp3": {"meta": {"recordingid": self.file2id}}}
 
@@ -46,7 +52,8 @@ class CollectionTest(TestCase):
 
     @mock.patch("os.listdir")
     @mock.patch("compmusic.file_metadata")
-    def test_check_existing_dirs_file_changed(self, meta, listdir):
+    @mock.patch("dashboard.jobs._get_mp3_files")
+    def test_check_existing_dirs_file_changed(self, get_mp3_files, meta, listdir):
         newuuid = str(uuid.uuid4())
 
         def metadata_side(path, *args, **kwargs):
@@ -54,7 +61,10 @@ class CollectionTest(TestCase):
                 return {"meta": {"recordingid": self.file1id}}
             elif path == "/a/directory/audio/subdir/two.mp3":
                 return {"meta": {"recordingid": newuuid}}
-        listdir.return_value = ["one.mp3", "two.mp3"]
+
+        files = ["one.mp3", "two.mp3"]
+        listdir.return_value = files
+        get_mp3_files.return_value = files
         meta.side_effect = metadata_side
 
         jobs._check_existing_directories(self.collection)
@@ -65,12 +75,15 @@ class CollectionTest(TestCase):
     @mock.patch("dashboard.jobs._create_collectionfile")
     @mock.patch("os.listdir")
     @mock.patch("dashboard.jobs._get_musicbrainz_release_for_dir")
-    def test_match_directory_to_release(self, get_mbrel, listdir, create_cf):
+    @mock.patch("dashboard.jobs._get_mp3_files")
+    def test_match_directory_to_release(self, get_mp3_files, get_mbrel, listdir, create_cf):
         relid = str(uuid.uuid4())
         models.MusicbrainzRelease.objects.create(collection=self.collection, mbid=relid)
 
         get_mbrel.return_value = [relid]
-        listdir.return_value = ["one.mp3", "two.mp3"]
+        files = ["one.mp3", "two.mp3"]
+        listdir.return_value = files
+        get_mp3_files.return_value = files
 
         jobs._match_directory_to_release(self.collection.collectionid, "/a/directory/audio/sub")
         cd = models.CollectionDirectory.objects.get(collection=self.collection, path="sub")
@@ -81,7 +94,9 @@ class CollectionTest(TestCase):
 
         # Now that it's run once, we can get the CollectionDirectory and try
         # adding another file
-        listdir.return_value = ["one.mp3", "two.mp3", "three.mp3"]
+        files = ["one.mp3", "two.mp3", "three.mp3"]
+        listdir.return_value = files
+        get_mp3_files.return_value = files
         create_cf.reset_mock()
 
         jobs._match_directory_to_release(self.collection.collectionid, "/a/directory/audio/sub")
@@ -111,17 +126,27 @@ class CollectionTest(TestCase):
 
     @mock.patch("os.listdir")
     @mock.patch("compmusic.file_metadata")
-    def test_get_musicbrainz_release_for_dir(self, meta, listdir):
+    @mock.patch("dashboard.jobs._get_mp3_files")
+    def test_get_musicbrainz_release_for_dir(self, get_mp3_files, meta, listdir):
+        files = ["one.mp3", "two.mp3"]
         releaseid = str(uuid.uuid4())
-        listdir.return_value = ["one.mp3", "two.mp3"]
+        listdir.return_value = files
+        get_mp3_files.return_value = files
         meta.return_value = {"meta": {"releaseid": releaseid}}
 
         releases = jobs._get_musicbrainz_release_for_dir("/a/dir")
 
         self.assertEqual(releases[0], releaseid)
         listdir.assert_called_with("/a/dir")
+        get_mp3_files.assert_called_with("/a/dir", files)
 
-    def test_get_mp3_files(self):
+    @mock.patch("os.path.isfile")
+    @mock.patch("magic.from_file")
+    def test_get_mp3_files(self, from_file, is_file):
+        is_file.return_value = True
+        from_file.side_effect = ["audio/mpeg", "audio/wav", "audio/flac", "audio/mpeg", "audio/mpeg"]
+
         files = ["one.mp3", "two.wav", "three.flac", "four.mp3", "five.mp3"]
-        out = jobs._get_mp3_files(files)
+        dirname = "/path"
+        out = jobs._get_mp3_files(dirname, files)
         self.assertEqual(out, ["one.mp3", "four.mp3", "five.mp3"])
